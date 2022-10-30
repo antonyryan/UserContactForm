162fe1191c2bdc755befbf8e1280e4803b632199
antonyryan <antonyryanr@gmail.com>
2022-10-30 01:56:34 -0300

    Implement submission success page

---
+++ frontend/src/routes/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <h1>
+            hello world!!!!
+        </h1>
+    )
+}
+
+export default Home
---
+++ frontend/src/routes/SubmissionSuccess/SubmissionSuccess.jsx
@@ -0,0 +1,17 @@
+import Box from "@mui/material/Box"
+
+const SubmissionSuccess = () => {
+    return (
+        <Box
+            sx={{
+                textAlign: "center",
+                fontSize: "4rem",
+                marginTop: "300px"
+            }}
+        >
+            Thank you for your submission.
+        </Box>
+    )
+}
+
+export default SubmissionSuccess
