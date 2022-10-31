052496fc8a4e0c1e7e8fc496ba221618268688b6
antonyryan <antonyryanr@gmail.com>
2022-10-30 21:48:44 -0300

    Create user contact form

--- frontend/src/components/UserContactForm/UserContactForm.jsx
+++ frontend/src/components/UserContactForm/UserContactForm.jsx
@@ -1,3 +1,5 @@
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
 import { Field } from 'formik'
 import * as Yup from 'yup'
 
@@ -7,18 +9,35 @@ export const validationSchema = Yup.object().shape({
   first_name: Yup.string().required('This field is required!'),
   last_name: Yup.string().required('This field is required!'),
   email: Yup.string().required('This field is required!'),
-  us_state: Yup.string().required('This field is required!'),
+  // us_state: Yup.string().required('This field is required!'),
   zip_code: Yup.string().required('This field is required!'),
 })
 
 const UserContactForm = ({ handleSubmit }) => {
+  // console.log({handleSubmit})
   return (
     <form onSubmit={handleSubmit}>
-      <Field component={FormInput} htmlId="first_name" type="text" name="first_name" label="First Name" />
-      <Field component={FormInput} htmlId="last_name" type="text" name="last_name" label="First Name" />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Field component={FormInput} htmlId="first_name" type="text" name="first_name" label="First Name" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Field component={FormInput} htmlId="last_name" type="text" name="last_name" label="Last Name" />
+        </Grid>
+      </Grid>
       <Field component={FormInput} htmlId="email" type="email" name="email" label="Email" />
       {/* <Field component={FormInput} htmlId="first_name" type="text" name="first_name" label="First Name" /> */}
-      <Field component={FormInput} htmlId="zip_code" type="text" name="zip_code" label="Zip Code" />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Field component={FormInput} htmlId="us_state" type="text" name="us_state" label="US State" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Field component={FormInput} htmlId="zip_code" type="text" name="zip_code" label="Zip Code" />
+        </Grid>
+      </Grid>
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
     </form>
   )
 }
--- frontend/src/routes/Home/Home.jsx
+++ frontend/src/routes/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useCallback } from 'react'
 import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
+import axios from 'axios'
 
 import UserContactForm from '../../components/UserContactForm'
 
@@ -16,27 +18,40 @@ const initialValues = {
 export const validationSchema = Yup.object().shape({
   first_name: Yup.string().required('This field is required!'),
   last_name: Yup.string().required('This field is required!'),
-  email: Yup.string().required('This field is required!'),
+  email: Yup.string().required('This field is required!').email(),
   us_state: Yup.string().required('This field is required!'),
   zip_code: Yup.string().required('This field is required!'),
 })
 
 const Home = () => {
-  const handleSubmit = useCallback((a, b, c, d, e) => {
-    console.log({ a, b, c, d, e })
+  const handleSubmit = useCallback((payload, formActions) => {
+    console.log({ payload })
+    axios
+      .post('http://localhost:8000/api/create_user', payload)
+      .then(res => {
+        console.log({ res })
+      })
+      .catch(err => {
+        console.log({ err })
+      })
   }, [])
 
   return (
-    <Box>
-      GGGGGG
-      <Formik
-        component={UserContactForm}
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-        enableReinitialize
-      />
-    </Box>
+    <Container>
+      <Box
+        sx={{
+          padding: '100px 10%',
+        }}>
+        <Box>UserContactForm</Box>
+        <Formik
+          component={UserContactForm}
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+          enableReinitialize
+        />
+      </Box>
+    </Container>
   )
 }
 
