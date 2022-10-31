f24f860e595ec286299a2d2da96b5017e63ff8e0
antonyryan <antonyryanr@gmail.com>
2022-10-31 03:28:01 -0300

    Create Header

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { Route, Routes as Switch } from 'react-router-dom'
 
 import Home from './Home'
-import SubmissionSuccess from "./SubmissionSuccess"
+import SubmissionSuccess from './SubmissionSuccess'
+import Header from '../components/Header'
 
 function Routes() {
   return (
-    <Switch>
-      <Route exact path="user/new" element={<Home />} />
-      <Route exact path="user/submission_success" element={<SubmissionSuccess />} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="user/new" element={<Home />} />
+        <Route exact path="user/submission_success" element={<SubmissionSuccess />} />
+      </Switch>
+    </>
   )
 }
 
